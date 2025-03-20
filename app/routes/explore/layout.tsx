23e3981c6cc810b4592adae89e55ab55c3b7add4
Callum Kloos <Callumkloos@gmail.com>
2025-03-20 13:22:35 +0000

    setup and experimentation

---
+++ app/routes/explore/layout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router";
+
+export default function ExploreLayout() {
+    return (
+        <div className="bg-red-300">
+            <h2>YES BOYS</h2>
+            <Outlet />
+        </div>
+    )
+}
