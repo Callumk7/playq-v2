2322e647583621bca6895ca32a560a4e96c2bdb7
Callum Kloos <Callumkloos@gmail.com>
2025-03-23 13:57:32 +0000

    shadcn sidebar

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,111 +1,155 @@
-import { ChevronRight } from "lucide-react";
-
 import {
-	Collapsible,
-	CollapsibleContent,
-	CollapsibleTrigger,
-} from "~/components/ui/collapsible";
-import {
-	Sidebar,
-	SidebarContent,
-	SidebarGroup,
-	SidebarGroupContent,
-	SidebarGroupLabel,
-	SidebarHeader,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-	SidebarRail,
-} from "~/components/ui/sidebar";
-import { Link, useLocation } from "react-router";
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map as MapIcon,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
 
+import { NavMain } from "~/components/nav-main"
+import { NavExplore } from "~/components/nav-projects"
+import { NavUser } from "~/components/nav-user"
+import { TeamSwitcher } from "~/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "~/components/ui/sidebar"
+
+// This is sample data.
 const data = {
-	navMain: [
-		{
-			title: "Explore",
-			url: "/explore",
-			items: [
-				{
-					title: "Games",
-					url: "/explore/games",
-				},
-				{
-					title: "Playlists",
-					url: "/explore/playlists",
-				},
-				{
-					title: "People",
-					url: "/explore/people",
-				},
-			],
-		},
-		{
-			title: "Collection",
-			url: "/collection",
-			items: [
-				{
-					title: "Games",
-					url: "/collection/games",
-				},
-				{
-					title: "Playlists",
-					url: "/collection/playlists",
-				},
-			],
-		},
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
     {
-      title: "Friends",
-      url: "/friends",
-      items: []
-    }
-	],
-};
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Playlists",
+      url: "/playlists",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "All Playlists",
+          url: "/collection/playlists",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Games",
+      url: "/collection/games",
+      icon: Bot,
+      items: [
+        {
+          title: "My Games",
+          url: "/collection/games",
+        },
+        {
+          title: "Explorer",
+          url: "#",
+        },
+        {
+          title: "Quantum",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Documentation",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Introduction",
+          url: "#",
+        },
+        {
+          title: "Get Started",
+          url: "#",
+        },
+        {
+          title: "Tutorials",
+          url: "#",
+        },
+        {
+          title: "Changelog",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "General",
+          url: "#",
+        },
+        {
+          title: "Team",
+          url: "#",
+        },
+        {
+          title: "Billing",
+          url: "#",
+        },
+        {
+          title: "Limits",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const {pathname} = useLocation();
-
-	return (
-		<Sidebar {...props}>
-			<SidebarHeader>
-				<h2 className="text-sm p-2 font-bold">playQ</h2>
-			</SidebarHeader>
-			<SidebarContent className="gap-0">
-				{/* We create a collapsible SidebarGroup for each parent. */}
-				{data.navMain.map((item) => (
-					<Collapsible
-						key={item.title}
-						title={item.title}
-						defaultOpen
-						className="group/collapsible"
-					>
-						<SidebarGroup>
-							<SidebarGroupLabel
-								asChild
-								className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
-							>
-								<CollapsibleTrigger>
-									{item.title}{" "}
-									<ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
-								</CollapsibleTrigger>
-							</SidebarGroupLabel>
-							<CollapsibleContent>
-								<SidebarGroupContent>
-									<SidebarMenu>
-										{item.items.map((item) => (
-											<SidebarMenuItem key={item.title}>
-												<SidebarMenuButton asChild isActive={pathname === item.url}>
-													<Link to={item.url}>{item.title}</Link>
-												</SidebarMenuButton>
-											</SidebarMenuItem>
-										))}
-									</SidebarMenu>
-								</SidebarGroupContent>
-							</CollapsibleContent>
-						</SidebarGroup>
-					</Collapsible>
-				))}
-			</SidebarContent>
-			<SidebarRail />
-		</Sidebar>
-	);
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavExplore />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
 }
