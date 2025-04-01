3f1baaa9141226ad92477a844ab25b8274a7df65
Callum Kloos <Callumkloos@gmail.com>
2025-04-01 15:10:28 +0100

    rendering public playlists

--- app/routes/explore/playlists.tsx
+++ app/routes/explore/playlists.tsx
@@ -1,7 +1,19 @@
-export default function ExplorePlaylistsPage() {
-  return (
-    <div>
-      <h2>Explore Playlist Page</h2>
-    </div>
-  )
+import { getPlaylists } from "~/db/queries/playlist";
+import type { Route } from "./+types/playlists";
+
+export const loader = async () => {
+	const publicPlaylists = await getPlaylists(undefined, false);
+	return publicPlaylists;
+};
+
+export default function ExplorePlaylistsPage({ loaderData }: Route.ComponentProps) {
+	const publicPlaylists = loaderData;
+	return (
+		<div>
+			<h2>Explore Playlist Page</h2>
+			{publicPlaylists.map((playlist) => (
+				<div key={playlist.id}>{playlist.name}</div>
+			))}
+		</div>
+	);
 }
