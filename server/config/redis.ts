351662bdfe09c4170370a06b9ba4b3faeb2f4121
Callum Kloos <Callumkloos@gmail.com>
2025-06-15 15:01:38 +0100

    configure private redis url correctly for bull mq

--- server/config/redis.ts
+++ server/config/redis.ts
@@ -1,6 +1,12 @@
 import type { ConnectionOptions } from "bullmq";
 import { env } from "~/lib/env";
 
+const redisURL = new URL(env.REDIS_URL);
+
 export const redisConfig: ConnectionOptions = {
-	url: env.REDIS_URL,
+	family: 0,
+	host: redisURL.hostname,
+	port: Number(redisURL.port),
+	username: redisURL.username,
+	password: redisURL.password,
 };
